af9086cb95862af629eb2e3a118a34169104fd1b
Jesse <jesse.r.shipley@gmail.com>
2018-06-22 09:06:43 -0500

    changes test_helper

--- test/models/clearbit_test.rb
+++ test/models/clearbit_test.rb
@@ -1,14 +1,12 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-
 require "test_helper"
 
 class ClearbitTest < Minitest::Test
+
   def test_clearbit_exists
     assert ClearbitService.new
   end
 
-  def test_clearbit_can_access_domain_info
+  def test_clearbit_can_fetch_domain_info
     cb = ClearbitService.new
     info = cb.get_domain('Clearbit')
 
