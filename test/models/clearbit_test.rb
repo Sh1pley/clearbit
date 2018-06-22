8a7c0010eeb397e80e6ca73af5b63eab339fd542
Jesse <jesse.r.shipley@gmail.com>
2018-06-22 08:16:17 -0500

    test get_domain method

--- test/models/clearbit_test.rb
+++ test/models/clearbit_test.rb
@@ -7,4 +7,11 @@ class ClearbitTest < Minitest::Test
   def test_clearbit_exists
     assert Clearbit.new
   end
+
+  def test_clearbit_can_access_domain_info
+    cb = Clearbit.new
+    info = cb.get_domain('Clearbit')
+
+    refute info.empty
+  end
 end
