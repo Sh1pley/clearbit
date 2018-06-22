ebd45c164b15cab8d10507f02678f656e2d04a6a
Jesse <jesse.r.shipley@gmail.com>
2018-06-22 11:28:18 -0500

    adds plaid service, for client, and transactions

--- test/models/plaid_test.rb
+++ test/models/plaid_test.rb
@@ -4,4 +4,20 @@ class PlaidTest < Minitest::Test
   def test_plaid_service_exists
     assert PlaidService.new
   end
+
+  def test_plaid_can_return_client
+    plaid = PlaidService.new
+    client = plaid.get_client
+
+    refute client.nil?
+    assert_equal client.class, Plaid::Client
+  end
+
+  def test_plaid_client_has_transactions
+    plaid = PlaidService.new
+    client = plaid.get_client
+    transactions = PlaidService.get_transactions(client)
+
+    refute transactions.empty?
+  end
 end
