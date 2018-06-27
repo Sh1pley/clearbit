a98f0a9563ec274b0a099f3ce67284ef39b45565
Jesse <jesse.r.shipley@gmail.com>
2018-06-27 10:59:54 -0500

    add webpack config for handling css sheets

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
           cacheDirectory: true,
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
       }
     ]
   },
